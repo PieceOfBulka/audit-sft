617ade786a4834f97200fca9a751e6f665820e9b
Alex Frolov <2204929@gmail.com>
2026-07-22 18:01:48 +0300

    fix unsloth only memory optimisation because of dropout!=0

--- lora_peft/train.py
+++ lora_peft/train.py
@@ -79,7 +79,7 @@ model = FastLanguageModel.get_peft_model(
     target_modules=["q_proj", "k_proj", "v_proj", "o_proj",
                     "gate_proj", "up_proj", "down_proj"],
     lora_alpha=32,
-    lora_dropout=0.05,
+    # lora_dropout=0.05, # будет оптимизация только по памяти
     bias="none",
     use_gradient_checkpointing="unsloth",
     random_state=42,
@@ -141,10 +141,10 @@ collator = DataCollatorForSeq2Seq(
 training_args = TrainingArguments(
     output_dir="./lora",
     num_train_epochs=2,
-    per_device_train_batch_size=128,
-    per_device_eval_batch_size=128,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=4,
     gradient_accumulation_steps=2,
-    learning_rate=32e-4,
+    learning_rate=1e-4,
     warmup_ratio=0.03,
     bf16=(device == "cuda"),
     fp16=(device == "mps"),
