a712d4b95b96d3e9a449ed49e31a712ffea0cf7b
Alex Frolov <2204929@gmail.com>
2026-07-28 16:51:11 +0300

    feat: download LLM weights args parse

--- download_LLM_weights.py
+++ download_LLM_weights.py
@@ -1,11 +1,20 @@
 """Download model weights into /weights"""
 from huggingface_hub import snapshot_download
+import argparse
 
-REPO_NAME = 'Qwen/Qwen3-8B'
-WEIGHTS_DIR = f'weights/{REPO_NAME}'
+def parse() -> argparse.Namespace:
+    p = argparse.ArgumentParser(description='Скачать веса модели с HuggingFace')
+    p.add_argument(
+        '--model',
+        help='Название модели с HF'
+    )
+    return p.parse_args()
+
+args = parse()
+WEIGHTS_DIR = f'weights/{args.model}'
 
 try:
-    path = snapshot_download(repo_id=REPO_NAME, local_dir=WEIGHTS_DIR)
-    print(f'Веса модели {REPO_NAME} были скачены в {path}')
+    path = snapshot_download(repo_id=args.model, local_dir=WEIGHTS_DIR)
+    print(f'Веса модели {args.model} были скачены в {path}')
 except Exception as e:
     print('Во время скачивания весов произошла ошибка:',e)
